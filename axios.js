0a062867f8c6b7006fc9022a61538e28ff594930
Balwantkiraula <balwantkiraula123@gmail.com>
2022-12-30 22:22:10 +0530

    initial commit

--- axios.js
+++
@@ -1,9 +0,0 @@
-// GET Request
-function getTodos(){
-   axios({
-    method: 'get', 
-    url: 'https://www.codecademy.com/article/what-is-crud'
-   })
-   .then(res => console.log(res))
-   .catch(err => console.error(err));
-}
